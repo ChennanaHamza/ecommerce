678d4ba8bf9428e6a623f4566687258401567c8a
Hamza Chennana <ibm@ibms-mbp.casanearshore.ma.ibm.com>
2019-08-08 18:12:15 +0100

    Third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListproductComponent } from './listproduct/listproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FilterPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
